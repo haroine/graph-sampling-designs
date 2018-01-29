6cc14695f9e2aa3a6828dff386a8d82098156f3b
haroine <antoine.rebecq@m4x.org>
2018-01-29 14:59:55 -0500

    Simulations rewritten as functions

--- R/main.R
+++ R/main.R
@@ -1,31 +1,71 @@
 library(igraph)
 library(foreach)
 library(sampling)
-source("snowball.R")
+library(tidyverse)
+source("R/snowball.R")
 #### Model graph
-N <- 25
+N <- 10000
+nSimus_sample <- 100
+n <- 50
 
-set.seed(1005192119)
+# set.seed(1005192119)
 
-# g <- watts.strogatz.game(1, N, N*0.2, 0.4)
-# g <- barabasi.game(N)
+g <- watts.strogatz.game(1, N, N*0.2, 0.4)
+# g <- barabasi.game(N, directed = F)
 # g <- make_ring(N, circular = T)
 # g <- graph_from_adjacency_matrix(matrix(1,nrow=N,ncol=N))
-g <- forest.fire.game(N, 0.35, directed=F)
+# g <- forest.fire.game(N, 0.35, directed=F)
 # g %>% rewire(each_edge(p = .2, loops = FALSE))
 # get.adjacency(g)
 
-transitivity(g, "local")
-plot(g)
+# transitivity(g, "local")
+# plot(g)
 
 
 # Y <- rep(1,N)
 # Y <- alpha_centrality(g)
-X <- degree(g)
-Y <- X + rnorm(length(X), mean(X), sd(X))
+# X <- degree(g)
+# Y <- X + rnorm(length(X), mean(X), sd(X))
+
+stats_toestimate <- c(ecount(g), vcount(g), mean(degree(g)), 
+                      mean(alpha_centrality(g)),
+                      mean(transitivity(g, "local"), na.rm=T))
+names(stats_toestimate) <- c("order","size","degree",
+                             "centrality_mean",
+                             "clustering_mean")
 
 #### Sampling
-nSimus_sample <- 10
-n <- 10
+results_sample <- get_snowball_sim(g, n, nSimus_sample, 
+                 c("degree", "alpha_centrality", "clustering"), 
+                 list(degree(g), alpha_centrality(g), transitivity(g, "local")) )
 
-test <- snowball_sample(g, n)
+## Compute estimators stats
+
+estimators_stats <- results_sample %>% 
+  group_by(stat_name) %>% 
+  summarise_at(vars(stat_value, se_simple, se_snowball, 
+                    bias_simple, bias_snowball,
+                    size_simple, size_snowball, stat_disp), 
+               mean, na.rm = T) 
+
+estimators_stats$var_simple <- estimators_stats$se_simple - (estimators_stats$bias_simple)**2
+estimators_stats$cv_simple <- sqrt(estimators_stats$var_simple) / abs(estimators_stats$stat_value)
+
+estimators_stats$var_snowball <- estimators_stats$se_snowball - (estimators_stats$bias_snowball)**2
+estimators_stats$cv_snowball <- sqrt(estimators_stats$var_snowball) / abs(estimators_stats$stat_value)
+
+deff_denom <- function(n, N, s2) { return( (1-n/N)*s2/n  ) }
+estimators_stats$deff_simple <- estimators_stats$var_simple / 
+  deff_denom(estimators_stats$size_simple, N, estimators_stats$stat_disp)
+
+estimators_stats$deff_snowball <- estimators_stats$var_snowball / 
+  deff_denom(estimators_stats$size_snowball, N, estimators_stats$stat_disp)
+
+
+###### Induced subgraphs
+
+# g_test <- induced_subgraph(g, test$sample_first)
+# g_test2 <- induced_subgraph(g, test$sample_snowball)
+# plot(g)
+# plot(g_test)
+# plot(g_test2)
--- R/snowball.R
+++ R/snowball.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(foreach)
+
 ## TODO doc
 ## TODO output graph from snowball sample
 ## Only for 1-degree snowball sampling
@@ -43,4 +46,88 @@ snowball_sample <- function(g, n, method_first="bernoulli", X=NULL) {
   return(list(sample_snowball=sample_snowball, 
               weights=weights, sample_first=sample_first, 
               weights_first=weights_first))
+}
+
+
+
+#' Compute the sampling estimates for vertices stats
+#' @param graph_stat vector of size number of vertices,
+#' value for each vertex
+#' @param weights vector of size number of vertices, sampling
+#' weight for each vertex
+#' @param type_est "Hajek" or "HT"
+estimate_vertex_stat_mean <- function(graph_stat, selected_vertices,
+                                      weights, type_est, N=NULL) {
+  
+  if( type_est != "Hajek" && !is.null(N) ) {
+    size_est <- N
+  } else {
+    size_est <- sum(weights)
+  }
+  
+  return( sum(graph_stat[selected_vertices] * (weights), na.rm=T) / size_est )
+  
+}
+
+
+#' Return real statistic and estimators with simple sampling
+#' design and 1-snowball (useful for simulations)
+#' @param graph_stat function
+#' @param snowball_sample
+estimators_snowball <- function(graph_stat, snowball_sample, N) {
+  
+  return( c(mean(graph_stat, na.rm=T), 
+            
+            var(graph_stat, na.rm=TRUE),
+            
+            estimate_vertex_stat_mean(graph_stat, snowball_sample$sample_first,
+                                      snowball_sample$weights_first, type_est = "HT", N = N),
+            
+            length(snowball_sample$sample_first),
+            
+            estimate_vertex_stat_mean(graph_stat, snowball_sample$sample_snowball,
+                                      snowball_sample$weights, type_est = "HT", N = N),
+            
+            length(snowball_sample$sample_snowball)
+  ) )
+  
+}
+
+
+#' Simulations
+#' @param graph_stat list of vecs of size N with stat value for each vertex 
+get_snowball_sim <- function(g, n, nSimus_sample, name_stat, graph_stat) {
+  
+  ## TODO rewrite not using rbinds
+  
+  if(length(name_stat) != length(graph_stat)) {
+    stop("name_stat and graph_stat must have same lengths")
+  }
+  
+  df_sim_final <- NULL
+  
+  for(k in 1:length(name_stat)) {
+    
+    matrix_sims <-     foreach(j=1:nSimus_sample, .combine=rbind) %do% {
+      snowball_sample_obj <- snowball_sample(g, n)
+      c(name_stat[k], estimators_snowball(graph_stat[[k]], snowball_sample_obj, vcount(g)) )
+    }
+    
+    df_sim <- as_tibble(matrix_sims)
+    names(df_sim) <- c("stat_name","stat_value","stat_disp",
+                       "est_simple","size_simple","est_snowball","size_snowball")
+    
+    df_sim_final <- rbind(df_sim, df_sim_final)
+  }
+  
+  df_sim_final <- df_sim_final %>% mutate_at(vars(-stat_name), as.numeric)
+  
+  df_sim_final$se_simple <- (df_sim_final$est_simple - df_sim_final$stat_value)**2
+  df_sim_final$se_snowball <- (df_sim_final$est_snowball - df_sim_final$stat_value)**2
+  
+  df_sim_final$bias_simple <- df_sim_final$est_simple - df_sim_final$stat_value
+  df_sim_final$bias_snowball <- df_sim_final$est_snowball - df_sim_final$stat_value
+  
+  
+  return(df_sim_final)
 }
