d607e192ec1c8c4df1a6bc9ad63366fc4ec9f1dc
Antoine R <antoine.rebecq@m4x.org>
2018-02-04 14:01:40 -0500

    Add notes for plugin sampling

---
+++ R/plugin_sampling.R
@@ -0,0 +1,9 @@
+
+## TODO Do the same analysis for plugin sampling
+###### Induced subgraphs
+
+# g_test <- induced_subgraph(g, test$sample_first)
+# g_test2 <- induced_subgraph(g, test$sample_snowball)
+# plot(g)
+# plot(g_test)
+# plot(g_test2)
