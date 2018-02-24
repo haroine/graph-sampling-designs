d4f7f10c23f6736b8b87039b854f048b724a21b9
Antoine R <antoine.rebecq@m4x.org>
2018-02-24 14:56:31 -0500

    Add clean simulation datasets

---
+++ R/datasets.R
@@ -0,0 +1,27 @@
+
+#' @name forestfire_ambs
+#' @title 
+#' @description Used to compute snowball sample size with Bernoulli first 
+#' with ambs varying from 1 to 10 by 1
+NULL
+
+#' @name forestfire_fwprob_all
+#' @title 
+#' @description Used to compute snowball sample size with Bernoulli first
+#' with fwprobs from 0.05 to 0.95 by 0.05
+NULL
+
+#' @name forestfire_pps_all.rds
+#' @description Compute deffs for snowball with pps first with various auxiliary
+#' variables on forestfire with fwprobs varying from 0.01 to 0.30, by 0.01
+NULL
+
+#' @name forestfire_pps_centralities.rds
+#' @description Compute deffs for snowball with pps first with centralities as
+#' auxiliary variables on forestfire with fwprobs varying from 0.01 to 0.30, by 0.01
+NULL
+
+#' @name yeast_centralities.rds
+#' @description Compute deffs for snowball with pps first with centralities as
+#' auxiliary variables on yeast graph
+NULL
