960c224a347d425fe181fa93cc73c96fa846f0c1
haroine <antoine.rebecq@m4x.org>
2018-02-26 19:47:44 -0500

    Plots for snowball deffs

--- R/graphs_estimators.R
+++ R/graphs_estimators.R
@@ -1,25 +1,37 @@
 library(tidyverse)
 
-ff_estimators <- readRDS("data/forestfire_parameters.rds")
+ff_estimators <- readRDS("data/forestfire_snowball.rds")
 estimators_stats <- ff_estimators
 
-df_toplot <- estimators_stats %>% filter(name=="inv_clustering")
+df_toplot <- estimators_stats %>% filter(name=="eigen")
+# df_toplot$testvar <- df_toplot$deff_snowball / df_toplot$deff_simple
 # df_toplot <- estimators_stats
+
+df_toplot <- df_toplot  %>%
+  arrange(deff_snowball)
+
+## Order by stat value in the plot
+levels_stat_name <- rev(unique(df_toplot$stat_name))
+df_toplot$stat_name <- factor(df_toplot$stat_name, levels=levels_stat_name)
 
 plot_deff <- ggplot(data = df_toplot
                     , aes(x=parameter)) +
-  geom_line(aes(y=deff_snowball, colour=stat_name)) +
   # geom_line(aes(y=deff_simple, colour=stat_name)) +
+  geom_line(aes(y=deff_snowball, colour=stat_name)) +
   geom_hline(yintercept = 1, linetype="longdash") +
-  scale_y_log10()
+  scale_y_log10() +
+  labs(title = "Design effect for snowball sampling",
+       subtitle = "For a forest-fire networkof order 1000, initial sample drawn by Bernoulli of expected size 50",
+       caption = "Ambassadors parameter for forest-fire = 1", 
+       x = "fwprobs", y = "Deff") 
 
 print(plot_deff)
 
 ## TODO does pps sampling improve things for initial sample size?
-
-## Size for Bernoulli initial sample (no differentiation between stats)
-plot_size <- ggplot(data = estimators_stats, 
-                    aes(x=parameter)) +
-  geom_line(aes(y=size_snowball, colour=stat_name))
-
-print(plot_size)
+# 
+# ## Size for Bernoulli initial sample (no differentiation between stats)
+# plot_size <- ggplot(data = estimators_stats, 
+#                     aes(x=parameter)) +
+#   geom_line(aes(y=size_snowball, colour=stat_name))
+# 
+# print(plot_size)
