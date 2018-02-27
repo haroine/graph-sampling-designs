7673ee32a4888d438ec0e2c257d215eac74943a9
Antoine R <antoine.rebecq@m4x.org>
2018-02-27 18:32:42 -0500

    Tidy deff estimates dataset

--- R/formatting_metric_neighbor_dispersion.R
+++ R/formatting_metric_neighbor_dispersion.R
@@ -1,4 +1,15 @@
 
 metrics_real <- readRDS("data/deffs_networks.rds")
 
-test <- gather(metrics_real)
+deff_tidy <- metrics_real %>% 
+  gather(deff1, deff2, 2:ncol(metrics_real)) %>%
+  separate(deff1, into="var", sep="_", extra="drop", remove=F) %>%
+  mutate(deff1 = gsub("deff[1:2]_", "", deff1))
+
+names(deff_tidy) <- c("graph_name","var","metric","deff")
+
+deff_tidy <- deff_tidy %>%
+  mutate(deff=as.numeric(deff)) %>%
+  arrange(graph_name, deff)
+
+saveRDS(deff_tidy, "data/tidy_deffs_networks.rds")
