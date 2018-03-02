f5cdde168a44132d12679070592d4d88bfa93e9d
Antoine R <antoine.rebecq@m4x.org>
2018-03-02 08:19:58 -0500

    Correction on deff2 formula

--- R/formatting_metric_neighbor_dispersion.R
+++ R/formatting_metric_neighbor_dispersion.R
@@ -15,3 +15,45 @@ deff_tidy <- deff_tidy %>%
 deff_tidy$deff <- deff_tidy$deff + 1
 
 saveRDS(deff_tidy, "data/tidy_deffs_networks.rds")
+
+#### Correction (previously forgot to multiply by mean degree in formula)
+
+deff_tidy <- readRDS("data/tidy_deffs_networks.rds")
+
+corrected_deffs <- deff_tidy
+
+corrected_deffs$dbar <- 1
+
+set.seed(1005192119)
+
+N <- 1000
+
+
+corrected_deffs[corrected_deffs$graph_name=="0.05",]$dbar <- 
+  mean(degree(forest.fire.game(N, 0.05, directed=F)), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="0.1",]$dbar <- 
+  mean(degree(forest.fire.game(N, 0.10, directed=F)), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="0.15",]$dbar <- 
+  mean(degree(forest.fire.game(N, 0.15, directed=F)), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="0.2",]$dbar <- 
+  mean(degree(forest.fire.game(N, 0.20, directed=F)), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="0.25",]$dbar <- 
+  mean(degree(forest.fire.game(N, 0.25, directed=F)), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="0.3",]$dbar <- 
+  mean(degree(forest.fire.game(N, 0.30, directed=F)), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="BA",]$dbar <- 
+  mean(degree(barabasi.game(N, directed = F)), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="immuno",]$dbar <- 
+  mean(degree(immuno), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="USairports",]$dbar <- 
+  mean(degree(USairports), na.rm=T)
+corrected_deffs[corrected_deffs$graph_name=="yeast",]$dbar <- 
+  mean(degree(yeast), na.rm=T)
+
+corrected_deffs[corrected_deffs$metric == "deff2",]$deff <- 
+(corrected_deffs[corrected_deffs$metric == "deff2",]$deff - 1) * corrected_deffs[corrected_deffs$metric == "deff2",]$dbar + 1
+
+corrected_deffs$dbar <- NULL
+
+saveRDS(corrected_deffs, "data/tidy_deffs_networks.rds")
+
